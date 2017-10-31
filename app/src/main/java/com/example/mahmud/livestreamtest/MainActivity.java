b8f779e58e2b2c33ff2d286834f558f5b48b932b
mahmudurG5 <mahmudur@netg5.com>
2017-10-31 11:37:23 +0600

    Initail commit for mp4 video stream

---
+++ app/src/main/java/com/example/mahmud/livestreamtest/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.mahmud.livestreamtest;
+
+import android.app.ProgressDialog;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ProgressDialog mDialog;
+    VideoView videoView;
+    ImageButton btnPlayPause;
+
+    String videoUrl = "http://clips.vorwaerts-gmbh.de/VfE_html5.mp4";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        videoView = (VideoView) findViewById(R.id.videoView);
+        btnPlayPause = (ImageButton) findViewById(R.id.btn_play_pause);
+        btnPlayPause.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        mDialog = new ProgressDialog(MainActivity.this);
+        mDialog.setMessage("Please wait...");
+        mDialog.setCanceledOnTouchOutside(false);
+        mDialog.show();
+        try {
+            if (!videoView.isPlaying()) {
+                Uri uri = Uri.parse(videoUrl);
+                videoView.setVideoURI(uri);
+                videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        btnPlayPause.setImageResource(R.drawable.ic_play);
+                    }
+                });
+            } else {
+                videoView.pause();
+                btnPlayPause.setImageResource(R.drawable.ic_play);
+            }
+        } catch (Exception ex) {
+
+        }
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mDialog.dismiss();
+                mediaPlayer.setLooping(true);
+                videoView.start();
+                btnPlayPause.setImageResource(R.drawable.ic_pause);
+            }
+        });
+    }
+}
